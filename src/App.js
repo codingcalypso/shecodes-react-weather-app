f9d4858db1a65ac4390a9805327bc611a2bab80d
codingcalypso <101667609+codingcalypso@users.noreply.github.com>
2022-06-27 20:00:51 -0500

    Added Github URL

--- src/App.js
+++ src/App.js
@@ -2,5 +2,20 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">SheCodes Weather App</div>;
+  return (
+    <div className="App">
+      <h1>SheCodes Weather App</h1>
+      <footer>
+        This project was coded by Cristal and is {""}
+        <a
+          href="https://github.com/codingcalypso/shecodes-react-weather-app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          open-sourced on Github
+        </a>
+        .
+      </footer>
+    </div>
+  );
 }
