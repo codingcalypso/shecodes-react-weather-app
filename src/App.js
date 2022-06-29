710ac2d01079a57f01912257c13a8d2138f1169b
codingcalypso <101667609+codingcalypso@users.noreply.github.com>
2022-06-29 13:11:56 -0500

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -4,18 +4,20 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>SheCodes Weather App</h1>
-      <footer>
-        This project was coded by Cristal and is {""}
-        <a
-          href="https://github.com/codingcalypso/shecodes-react-weather-app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open-sourced on Github
-        </a>
-        .
-      </footer>
+      <div classNAme="container">
+        <h1>SheCodes Weather App</h1>
+        <footer>
+          This project was coded by Cristal and is {""}
+          <a
+            href="https://github.com/codingcalypso/shecodes-react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on Github
+          </a>
+          .
+        </footer>
+      </div>
     </div>
   );
 }
